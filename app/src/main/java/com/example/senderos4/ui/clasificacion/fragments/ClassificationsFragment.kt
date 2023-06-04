c01eb673468b59bbfbff11df40114cd5cdf05d0d
Fabio-Alfredo <00210121@uca.edu.sv>
2023-06-03 23:24:39 -0600

    navigation and presentation of rankings

--- app/src/main/java/com/example/senderos4/ui/clasificacion/fragments/ClassificationsFragment.kt
+++ app/src/main/java/com/example/senderos4/ui/clasificacion/fragments/ClassificationsFragment.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.TextureView
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.RecyclerView
@@ -40,20 +41,19 @@ class ClassificationsFragment : HiddenMenuFragment() {
                 0->{
                     view.findViewById<RecyclerView>(R.id.recyclerView).adapter = ClassificationAdapter().apply {
                         submitData(viewModel.getHeaders(), viewModel.getTop())
-                        view.findViewById<TextView>(R.id.text_liga).text = "liga 1"
-
+                        view.findViewById<ImageView>(R.id.division).setImageResource(R.drawable.division_bronce)
                     }
                 }
                 1->{
                     view.findViewById<RecyclerView>(R.id.recyclerView).adapter = ClassificationAdapter().apply {
                         submitData(viewModel.getHeaders(), viewModel.getTop())
-                        view.findViewById<TextView>(R.id.text_liga).text = "liga 2"
+                        view.findViewById<ImageView>(R.id.division).setImageResource(R.drawable.division_oro)
                     }
                 }
                 2->{
                     view.findViewById<RecyclerView>(R.id.recyclerView).adapter = ClassificationAdapter().apply {
                         submitData(viewModel.getHeaders(), viewModel.getTop())
-                        view.findViewById<TextView>(R.id.text_liga).text = "liga 3"
+                        view.findViewById<ImageView>(R.id.division).setImageResource(R.drawable.division_rubi)
                     }
                 }
             }
--- app/src/main/java/com/example/senderos4/ui/clasificacion/fragments/ContainerFragment.kt
+++ app/src/main/java/com/example/senderos4/ui/clasificacion/fragments/ContainerFragment.kt
@@ -7,6 +7,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.ImageButton
+import android.widget.ImageView
 import android.widget.TableLayout
 import androidx.fragment.app.activityViewModels
 import androidx.fragment.app.viewModels
@@ -26,7 +29,9 @@ class ContainerFragment : HiddenMenuFragment() {
 
     private lateinit var demoCollectionAdapter: DemoCollectionAdapter
     private lateinit var viewPager2: ViewPager2
-    private lateinit var tabLayout: TabLayout
+    //private lateinit var tabLayout: TabLayout
+    private lateinit var preButton: ImageView
+    private lateinit var nexButton:ImageView
 
 
     override fun onCreateView(
@@ -35,6 +40,84 @@ class ContainerFragment : HiddenMenuFragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_container, container, false)
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        bind()
+        addListener()
+
+        /*TabLayoutMediator(tabLayout, viewPager2) { tab, position ->
+            when (position) {
+                0 -> {
+                    tab.setIcon(R.drawable.liga_senderos)
+                }1->{
+                    tab.setIcon(R.drawable.icon_user_item)
+                }
+                2->{
+                    tab.setIcon(R.drawable.info_settings)
+                }
+            }
+        }.attach()*/
+    }
+
+
+    fun bind(){
+        viewPager2 = requireView().findViewById(R.id.pager)
+        //tabLayout = requireView().findViewById(R.id.movimiento)
+        nexButton = requireView().findViewById(R.id.nextButton)
+        preButton = requireView().findViewById(R.id.prevButton)
+
+        demoCollectionAdapter = DemoCollectionAdapter(this)
+        viewPager2.adapter = demoCollectionAdapter
+    }
+
+    fun addListener(){
+        preButton.setOnClickListener {
+            viewPager2.setCurrentItem(viewPager2.currentItem - 1, true)
+        }
+
+        nexButton.setOnClickListener {
+            viewPager2.setCurrentItem(viewPager2.currentItem + 1, true)
+        }
+
+        viewPager2.registerOnPageChangeCallback(object: ViewPager2.OnPageChangeCallback(){
+            override fun onPageSelected(position: Int) {
+
+                preButton.visibility = if(position == 0) {
+                    View.GONE
+                }
+                else{
+                    View.VISIBLE
+                }
+
+                nexButton.visibility = if(position == 2 ){
+                    View.GONE
+                }
+                else{
+                    View.VISIBLE
+                }
+            }
+        })
+    }
+
+    companion object{
+        private const val ARG_OBJECT = "object"
+    }
+
+    class DemoCollectionAdapter(fragment: Fragment):FragmentStateAdapter(fragment){
+        override fun getItemCount(): Int = 3
+
+        override fun createFragment(position: Int): Fragment {
+            val fragment = ClassificationsFragment()
+            fragment.arguments = Bundle().apply {
+                putInt(ARG_OBJECT, position)
+            }
+            return fragment
+        }
+
 
     }
 
@@ -59,52 +142,6 @@ class ContainerFragment : HiddenMenuFragment() {
 
     }*/
 
-    companion object{
-        private const val ARG_OBJECT = "object"
-    }
 
-    class DemoCollectionAdapter(fragment: Fragment):FragmentStateAdapter(fragment){
-        override fun getItemCount(): Int = 3
-
-        override fun createFragment(position: Int): Fragment {
-            val fragment = ClassificationsFragment()
-            fragment.arguments = Bundle().apply {
-                putInt(ARG_OBJECT, position)
-            }
-            return fragment
-        }
-
-
-    }
-
-
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-
-        bind()
-
-        TabLayoutMediator(tabLayout, viewPager2) { tab, position ->
-            when (position) {
-                0 -> {
-                    tab.setIcon(R.drawable.liga_senderos)
-                }1->{
-                    tab.setIcon(R.drawable.icon_user_item)
-                }
-                2->{
-                    tab.setIcon(R.drawable.info_settings)
-                }
-            }
-        }.attach()
-    }
-
-
-    fun bind(){
-        viewPager2 = requireView().findViewById(R.id.pager)
-        tabLayout = requireView().findViewById(R.id.movimiento)
-        
-        demoCollectionAdapter = DemoCollectionAdapter(this)
-        viewPager2.adapter = demoCollectionAdapter
-    }
 
 }
