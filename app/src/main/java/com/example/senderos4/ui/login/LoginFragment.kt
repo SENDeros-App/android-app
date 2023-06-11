55563c0d6e3b6f778ff47cd157c0d4c13f89e06b
Fabio-Alfredo <00210121@uca.edu.sv>
2023-06-11 15:10:48 -0600

    algo

--- app/src/main/java/com/example/senderos4/ui/login/LoginFragment.kt
+++ app/src/main/java/com/example/senderos4/ui/login/LoginFragment.kt
@@ -7,7 +7,6 @@ import android.view.ViewGroup
 import android.widget.Button
 import android.widget.Toast
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.viewModels
 import com.example.senderos4.R
 import com.example.senderos4.ui.login.viewmodels.LoginViewModel
 import com.google.android.material.textfield.TextInputEditText
