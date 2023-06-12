f5b710c023f9ca9a7dbe8bdeb30a5700795e3416
Fabio-Alfredo <00210121@uca.edu.sv>
2023-06-12 15:15:33 -0600

    authservice

--- app/src/main/java/com/example/senderos4/network/service/AuthService.kt
+++ app/src/main/java/com/example/senderos4/network/service/AuthService.kt
@@ -3,10 +3,14 @@ package com.example.senderos4.network.service
 import android.net.Credentials
 import com.example.senderos4.network.dto.login.LoginRequest
 import com.example.senderos4.network.dto.login.LoginResponse
+import com.example.senderos4.network.dto.register.RegisterRequest
 import retrofit2.http.Body
 import retrofit2.http.POST
 
 interface AuthService {
     @POST("")
     suspend fun login(@Body credentials: LoginRequest):LoginResponse
+
+    @POST("")
+    suspend fun register(@Body credentials: RegisterRequest):LoginResponse
 }
