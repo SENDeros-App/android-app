dc9ebb4186a52a11419198fa6ae74d5868b7a3f8
Fabio-Alfredo <00210121@uca.edu.sv>
2023-06-12 15:12:16 -0600

    authservice

--- app/src/main/java/com/example/senderos4/network/retrofit/Retrofit.kt
+++ app/src/main/java/com/example/senderos4/network/retrofit/Retrofit.kt
@@ -1,14 +1,24 @@
 package com.example.senderos4.network.retrofit
 
+import com.example.senderos4.network.service.AuthService
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
 const val URL = ""
 
-object Retrofit {
+object RetrofitInstance {
     private var token = ""
 
     fun setToken(token: String) {
         this.token = token
     }
 
+    private val retrofit = Retrofit.Builder()
+        .baseUrl(URL)
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
 
-
+    fun getLoginService(): AuthService{
+        return retrofit.create(AuthService::class.java)
+    }
 }
